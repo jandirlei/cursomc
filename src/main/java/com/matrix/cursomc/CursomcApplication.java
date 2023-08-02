9ccbb2c88d7af4a82f2d8e0d0158f5122a520417
jandirlei <jandirlei@yahoo.com.br>
2023-08-01 21:29:58 -0300

    Cliente, TipoCliente, Telefone e enderecos

--- src/main/java/com/matrix/cursomc/CursomcApplication.java
+++ src/main/java/com/matrix/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.matrix.cursomc.domain.Categoria;
 import com.matrix.cursomc.domain.Cidade;
+import com.matrix.cursomc.domain.Cliente;
+import com.matrix.cursomc.domain.Endereco;
 import com.matrix.cursomc.domain.Estado;
 import com.matrix.cursomc.domain.Produto;
+import com.matrix.cursomc.domain.enums.TipoCliente;
 import com.matrix.cursomc.repositories.CategoriaRepository;
 import com.matrix.cursomc.repositories.CidadeRepository;
+import com.matrix.cursomc.repositories.ClienteRepository;
+import com.matrix.cursomc.repositories.EnderecoRepository;
 import com.matrix.cursomc.repositories.EstadoRepository;
 import com.matrix.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -55,7 +66,6 @@ public class CursomcApplication implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
-		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
@@ -68,6 +78,19 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gamil.com", "36378912377", TipoCliente.PESSOAFISICA );
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 }
--- src/main/java/com/matrix/cursomc/domain/Produto.java
+++ src/main/java/com/matrix/cursomc/domain/Produto.java
@@ -18,7 +18,6 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
